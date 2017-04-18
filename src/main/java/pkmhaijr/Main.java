6ff82c947d136307f2ff3fdd98951b460dc37c03
Patryk Kacperski <patryk.kacperski141095@gmail.com>
2017-04-18 13:47:03 +0200

    Added Spring Boot to project

---
+++ src/main/java/pkmhaijr/Main.java
@@ -0,0 +1,17 @@
+package pkmhaijr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Asasello on 08-Mar-17.
+ */
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+}
---
+++ src/test/java/pkmhaijr/TestClass.java
@@ -0,0 +1,21 @@
+package pkmhaijr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Asasello on 02-Mar-17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestClass {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
