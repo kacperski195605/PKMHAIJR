2cddb206f4b2586768fa1df3479936315233d777
Jaroslaw Rolinski <asasello225@gmail.com>
2017-04-19 13:55:56 +0200

    Added DatabaseFacade - empty.

---
+++ src/main/java/pkmhaijr/managers/DatabaseFacade.java
@@ -0,0 +1,11 @@
+package pkmhaijr.managers;
+
+/**
+ * Created by Asasello on 19-Apr-17.
+ */
+public class DatabaseFacade {
+
+    private DatabaseFacade(){}
+
+    public DatabaseFacade getInstance(){return new DatabaseFacade();}
+}
