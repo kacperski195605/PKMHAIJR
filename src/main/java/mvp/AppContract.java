26843d4dcfa4b7fef503c4f2320ff913dab152ba
Jaroslaw Rolinski <asasello225@gmail.com>
2017-04-23 11:46:51 +0200

    Update Presenter interface.

--- src/main/java/mvp/AppContract.java
+++ src/main/java/mvp/AppContract.java
@@ -2,6 +2,9 @@ package mvp;
 
 import pkmhaijr.App;
 import pkmhaijr.model.ErrorType;
+import pkmhaijr.model.FilterType;
+
+import java.util.ArrayList;
 
 /**
  * Created by Asasello on 22-Apr-17.
@@ -10,9 +13,11 @@ public interface AppContract {
     public interface View{
         void setCurrentUser(App.User user);
         void error(ErrorType errorType);
+
     }
 
     public interface Presenter{
         void getCurrentUser();
+        void getProducts(FilterType type);
     }
 }
