d5e75b71d6d586441c6b8531f6adbbdefe366d03
Jaroslaw Rolinski <asasello225@gmail.com>
2017-05-08 18:34:52 +0200

    Travis fix 1.5

--- src/main/java/pkmhaijr/Main.java
+++ src/main/java/pkmhaijr/Main.java
@@ -2,6 +2,9 @@ package pkmhaijr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.sql.Timestamp;
 import java.util.Calendar;
@@ -11,6 +14,9 @@ import java.util.Calendar;
  */
 
 @SpringBootApplication
+@ComponentScan({"pkmhaijr"})
+@EntityScan("pkmhaijr")
+@EnableJpaRepositories("pkmhaijr")
 public class Main {
 
     public static void main(String[] args) {
--- src/main/java/pkmhaijr/controller/FacebookController.java
+++ src/main/java/pkmhaijr/controller/FacebookController.java
@@ -1,17 +1,13 @@
 package pkmhaijr.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
-import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
 
@@ -29,7 +25,7 @@ public class FacebookController {
         this.facebook = facebook;
         this.connectionRepository = connectionRepository;
     }
-    
+
     @GetMapping
     public String helloFacebook(Model model) {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
